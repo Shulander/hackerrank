571ca4dbbb464ad5989c25d9e7f1ef750d4a42fe
Henrique Vicentini <henrique@vicentini.us>
2020-03-03 20:17:50 -0800

    Java Regex 2 - Duplicate Words https://www.hackerrank.com/challenges/duplicate-word/problem

---
+++ src/test/java/us/vicentini/hackerrank/java/strings/DuplicateWordsTest.java
@@ -0,0 +1,23 @@
+package us.vicentini.hackerrank.java.strings;
+
+import org.junit.jupiter.api.Test;
+import us.vicentini.hackerrank.BaseSystemStreamOverride;
+
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DuplicateWordsTest extends BaseSystemStreamOverride {
+
+    @Test
+    public void testCase01() {
+        InputStream inputStream = getResourceAsStream("DuplicateWords_01_input.txt");
+        InputStream expectedStream = getResourceAsStream("DuplicateWords_01_output.txt");
+        provideInput(inputStream);
+
+        DuplicateWords.main(new String[0]);
+
+        assertInputStreams(expectedStream, getOutputAsInputStream());
+        assertTrue(true);
+    }
+}
